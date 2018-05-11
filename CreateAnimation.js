337ffb2d224daa0134e3267b411e72369675c9cc
Lizet N Reyes Rodas <lr40247@srcs.loc>
2018-05-11 10:39:46 -0700

    new ideas

--- CreateAnimation.js
+++ CreateAnimation.js
@@ -1,1 +1,20 @@
-gzdga
+var box = document.getElementById("animation1");
+var xpos = 0;
+var ypos = 0;
+var xstep = 1;
+var ystep = 1;
+var id = setInterval(frame, 5);
+
+function frame() {
+    if (xpos > 350 || xpos < 0) {
+        xstep = -xstep;
+    }
+     if (ypos > 350 || ypos < 0) {
+        ystep = -ystep;
+    }
+
+    xpos = xpos + xstep;
+    ypos = ypos + ystep;
+    box.style.top = ypos + 'px';
+    box.style.left = xpos + 'px';
+}
