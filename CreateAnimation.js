3e2f7abef8daae39fc96760accc4819818513ee5
Lizet N Reyes Rodas <lr40247@srcs.loc>
2018-05-17 12:19:44 -0700

    almost done

--- CreateAnimation.js
+++ CreateAnimation.js
@@ -1,39 +1,55 @@
-var box = document.getElementById("animation1");
-var xpos = 0;
-var ypos = 0;
-var xstep = 1;
-var ystep = 1;
-var id = setInterval(frame, 5);
+var box1 = document.getElementById("animation1");
+var xpos1 = 0;
+var ypos1 = 0;
+var xstep1 = 2;
+var ystep1 = 1;
+
+var box2 = document.getElementById("animation2");
+var xpos2 = 0;
+var ypos2 = 500;
+var xstep2 = 3;
+var ystep2 = 2;
+
+var box3 = document.getElementById("animation3");
+var xpos3 = 0;
+var ypos3 = 200;
+var xstep3 = 3;
+var ystep3 = 2;
+var id = setInterval(frame, 15);
 
 function frame() {
-    if (xpos > 350 || xpos < 0) {
-        xstep = -xstep;
-    }
-     if (ypos > 350 || ypos < 0) {
-        ystep = -ystep;
+
+    if (xpos1 > 350 || xpos1 < 0) {
+        xstep1 = -xstep1;
     }
 
-    xpos = xpos + xstep;
-    ypos = ypos + ystep;
-    box.style.top = ypos + 'px';
-    box.style.left = xpos + 'px';
+
+    if (xpos2 > 350 || xpos2 < 0) {
+        xstep2 = -xstep2;
+    }
+    if (ypos2 > 600 || ypos2 < 0) {
+        ystep2 = -ystep2;
+    }
+
+    if (xpos3 > 350 || xpos3 < 0) {
+        xstep3 = -xstep3;
+    }
+    if (ypos3 > 350 || ypos3 < 0) {
+        ystep3 = -ystep3;
+    }
+
+    xpos1 = xpos1 + xstep1;
+    box1.style.left = xpos1 + 'px';
+
+
+    xpos2 = xpos2 + xstep2;
+    ypos2 = ypos2 + ystep2;
+    box2.style.top = ypos2 + 'px';
+    box2.style.left = xpos2 + 'px';
+
+     xpos3 = xpos3 + xstep3;
+    ypos3 = ypos3 + ystep3;
+    box3.style.top = ypos3 + 'px';
+    box3.style.left = xpos3 + 'px';
 }
 
-var box = document.getElementById("animation2");
-var xpos = 0;
-
-var xstep = 1;
-
-var id = setInterval(frame, 5);
-
-function frame() {
-    if (xpos > 350 || xpos < 0) {
-        xstep = -xstep;
-    }
-
-
-    xpos = xpos + xstep;
-
-
-    box.style.left = xpos + 'px';
-}
